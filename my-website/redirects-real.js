c20db16d7f690691642b0ed6c84ababbe0957512
Luke Russell <luke.russell@slack-corp.com>
2024-07-31 14:21:01 -0700

    go

--- my-website/redirects-real.js
+++ my-website/redirects-real.js
@@ -4,7 +4,6 @@ if (ExecutionEnvironment.canUseDOM) {
   (function() {
     // List of specific paths to redirect
     const redirects = {
-      '/docs-example/': '/docs-example/intro',
       '/docs-example/tutorials/intro': '/docs-example/intro',
     };
 
