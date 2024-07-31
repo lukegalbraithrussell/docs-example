9ef692edd4b85cfc0af3f848fb2b2154c51f2d43
Luke Russell <luke.russell@slack-corp.com>
2024-07-31 13:07:56 -0700

    false

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -11,6 +11,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
+  trailingSlash: false,
 
   // Set the production url of your site here
   url: 'https://lukegalbraithrussell.github.io',
