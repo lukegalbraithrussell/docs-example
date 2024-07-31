dad6870116bde7dd225b299fafe1f14e718a485a
Luke Russell <luke.russell@slack-corp.com>
2024-07-31 14:02:58 -0700

    gooooogogogogo

--- my-website/redirects.js
+++ my-website/redirects.js
@@ -90,17 +90,16 @@ if (ExecutionEnvironment.canUseDOM) {
     const currentHash = window.location.hash;
 
     // If there is a hash fragment
-    if (currentHash && currentHash.includes("#")) {
-      // Create the full URL with hash replaced by '/'
-      const newPath = currentPath + currentHash.replace("#", "/");
-
+    if (currentHash && currentHash.includes('#')) {
       // Loop through the list of URLs to handle
       for (const url of urlsToHandle) {
+        const urlPath = url.split('#')[0];
+        const urlHash = `#${url.split('#')[1]}`;
+
         // Check if the current path matches the URL to handle
-        if (
-          currentPath === url.split("#")[0] &&
-          window.location.hash === `#${url.split("#")[1]}`
-        ) {
+        if (currentPath === urlPath && currentHash === urlHash) {
+          let newPath = currentPath + '/' + currentHash.replace('#', '');
+
           // Redirect to the new path
           window.location.replace(newPath);
           return; // Exit after the first match
