0197c5593e45e2f9670e7318d5418ef3dfc0e90d
Luke Russell <luke.russell@slack-corp.com>
2024-08-01 15:38:42 -0700

    go

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -12,23 +12,16 @@ const config = {
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
   url: 'https://lukegalbraithrussell.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docs-example/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
   organizationName: 'lukegalbraithrussell', // Usually your GitHub org/user name.
   projectName: 'docs-example', // Usually your repo name.
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "warn",
+  onBrokenAnchors: "warn",
+  onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
+
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
