f86825b2fbdc68ff626fe58e74d806f1e28a7cc5
Luke Russell <luke.galbraith.russell@gmail.com>
2025-02-20 22:15:45 -0800

    change url test

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
-  url: 'https://docs.lukerussell.info',
+  url: 'https://docs.lukegalbraith.com',
   baseUrl: '/',
   organizationName: 'lukegalbraithrussell', // Usually your GitHub org/user name.
   projectName: 'docs-example', // Usually your repo name.
